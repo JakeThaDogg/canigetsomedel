7c2199484bb3d9d61dea6a5ff117924f6f33265f
Lucas Poirier <poirier.dev@gmail.com>
2018-04-12 13:47:28 +0200

    yay

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,9 @@ var router = express.Router()
 /* GET users listing. */
 router.get('/:input', function (req, res, next) {
   if (isNaN(req.params.input)) {
-    res.render('update-user')
+    res.render('update-user', {input: req.params.input})
   } else {
-    res.render('delete-user')
+    res.render('delete-user', {input: req.params.input})
   }
 })
 
@@ -19,7 +19,7 @@ router.put('/:name', function (req, res, next) {
 })
 
 router.delete('/:input', function (req, res, next) {
-  res.send('No more user with ID ' + req.body.input)
+  res.send('No more user with ID ' + req.params.input)
 })
 
 module.exports = router
